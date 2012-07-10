1b84ca5d1042f1150b52d96b4a103d6b5a93b9f1
Heber Bastidas <heberbastidas@gmail.com>
2012-07-10 17:52:02 -0430

    first commit

---
+++ README.txt
@@ -0,0 +1,6 @@
+Introduction
+============
+
+tipos de contenidos dados en curso de plone
+
+This product may contain traces of nuts.
---
+++ setup.py
@@ -0,0 +1,47 @@
+from setuptools import setup, find_packages
+import os
+
+version = '0.01'
+
+setup(name='misitio.contenttypes',
+      version=version,
+      description="tipos de contenido para mi sitio",
+      long_description=open("README.txt").read() + "\n" +
+                       open(os.path.join("docs", "HISTORY.txt")).read(),
+      # Get more strings from
+      # http://pypi.python.org/pypi?%3Aaction=list_classifiers
+      classifiers=[
+        "Framework :: Plone",
+        "Programming Language :: Python",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        ],
+      keywords='types content',
+      author='Heber Bastidas',
+      author_email='heberbastidas@gmail.com',
+      url='https://github.com/hbastidas/misitio.contenttypes',
+      license='GPL version 2',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['misitio'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+          'plone.app.dexterity',
+          'plone.namedfile [blobs]',
+          # -*- Extra requirements: -*-
+      ],
+      extras_require={
+        'test': ['plone.app.testing'],
+        },
+      entry_points="""
+      # -*- Entry points: -*-
+      [z3c.autoinclude.plugin]
+      target = plone
+      """,
+      # The next two lines may be deleted after you no longer need
+      # addcontent support from paster and before you distribute
+      # your package.
+      setup_requires=["PasteScript"],
+      paster_plugins = ["ZopeSkel"],
+
+      )
